caa48e1a6beec492b826cb7df4d22fcd24b7c038
Artem Antuh <artyomantuh423@mail.ru>
2018-08-29 03:41:15 +0300

    Second commit

--- Uber App For Driver/Controller/DriverVC.swift
+++ Uber App For Driver/Controller/DriverVC.swift
@@ -9,19 +9,61 @@
 import UIKit
 import MapKit
 
-class DriverVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-
+class DriverVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UberController {
+    
     
     @IBOutlet weak var myMap: MKMapView!
     
+    private var locationManager = CLLocationManagerDelegate
+    private var userLocation: CLLocationCoordinate2D?
+    //    private var riderLocation: CLLocationCoordinate2D?
+    
+    private var acceptedUber = false
+    private var driverCanceledUber = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        initializeLocationManager()
+        
+        UberHandler.Instance.delegate = self
+        UberHandler.Instance.observeMessagesForDriver()
+    }
+    
+    private func initializeLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
     
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        //if we have coordinates from the manager
+        if let location = locationManager.location?.coordinate {
+            
+            userLocation = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            //setting up navigation region
+            let region = MKCoordinateRegion(center: userLocation!, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            myMap.setRegion(region, animated: true)
+            myMap.removeAnnotations(myMap.annotations)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = userLocation!
+            annotation.title = "Drivers Loaction"
+            myMap.addAnnotation(annotation)
+        }
+        
+    }
+    func acceptUber(lat: Double, long: Double) {
+//        uberRequest(title: "Uber Request", message: "You have a request for an Uber for this location Lat: \(lat), Long: \(long)", requestAlive: true)
+        if !acceptedUber {
+            uberRequest(title: "Uber Request",
+                        message: "You have a request for an Uber for this location Lat: \(lat), Long: \(long)",
+                        requestAlive: true)
+        }
+    }
     
     @IBAction func cancelUber(_ sender: Any) {
     }
@@ -33,21 +75,48 @@ class DriverVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
         else
         {
             //problem
-            alertTheUser(title: "Could Not LogOut", message: "Try later")
+            uberRequest(title: "Could Not LogOut", message: "Try later", requestAlive: false)
+            
+            //            alertTheUser(title: "Could Not LogOut", message: "Try later")
         }
     }
     
-    private func alertTheUser(title: String, message: String) {
+    private func uberRequest(title: String,message: String,requestAlive: Bool) {
+        
         let alert = UIAlertController(title: title,
                                       message: message,
                                       preferredStyle: .alert);
-        
-        let ok = UIAlertAction(title: "OK",
-                               style: .default,
-                               handler: nil);
-        alert.addAction(ok)
+        if requestAlive {
+            let accept = UIAlertAction(title: "Accept", style: .default, handler: {(alertAction: UIAlertAction) in
+                
+                
+            })
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            
+            alert.addAction(accept)
+            alert.addAction(cancel)
+        }
+        else
+        {
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(ok)
+        }
         present(alert, animated: true, completion: nil)
+        
     }
     
-
+    //    private func alertTheUser(title: String, message: String) {
+    //        let alert = UIAlertController(title: title,
+    //                                      message: message,
+    //                                      preferredStyle: .alert);
+    //
+    //        let ok = UIAlertAction(title: "OK",
+    //                               style: .default,
+    //                               handler: nil);
+    //        alert.addAction(ok)
+    //        present(alert, animated: true, completion: nil)
+    //    }
+    
+    
 }
--- Uber App For Driver/Plugins/DBReference.swift
+++ Uber App For Driver/Plugins/DBReference.swift
@@ -25,6 +25,16 @@ class DBProvider {
         return dbRef.child(Constants.DRIVERS)
     }
     //request
+    var requestRef: DatabaseReference{
+        return dbRef.child(Constants.UBER_REQUEST)
+    }
+    
+    var requestAcceptedref: DatabaseReference {
+        return dbRef.child(Constants.UBER_ACCEPTED)
+    }
+    
+    
+    //request Accepted
     func saveUser(withID: String, email:String, paasword: String) {
         let data: Dictionary <String, Any> = [Constants.EMAIL : email,
                                               Constants.PASSWORD : paasword,
